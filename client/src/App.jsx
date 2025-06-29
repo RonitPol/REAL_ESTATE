e0c64af4cb252b3870f43f96143a395107ccc38e
RonitPol <ronit.17151@sakec.ac.in>
2025-06-29 18:36:38 +0530

    Added Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Signin from './pages/Signin';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Signup from './pages/Signup';
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
