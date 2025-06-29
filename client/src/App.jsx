8090a73d6735504b13cb2ec7f153062d09b76f5f
RonitPol <ronit.17151@sakec.ac.in>
2025-06-29 17:47:17 +0530

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter , Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Signup from './pages/Signup';
 export default function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
